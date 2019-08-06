8d2ff187eb63d70a7bdbb0a787bd0596f646682a
Melvin Löfgren <devopsmanta@gmail.com>
2019-08-06 12:54:58 +0800

    portfolio images

--- src/store/GameStore.js
+++ src/store/GameStore.js
@@ -4,8 +4,5 @@ import rootReducer from '../reducers/index';
 
 export default createStore(
   rootReducer,
-  compose(
-    applyMiddleware(promiseMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(promiseMiddleware))
 );
